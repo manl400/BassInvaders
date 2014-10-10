3d779d13db86803bc5bb0193340aaf77162079cc
manl400 <movie.edit@yahoo.com>
2014-10-10 15:02:00 -0500

    Ignore main.o

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*
+//	Bass Invaders
+//		By:
+//	Zain Ashraf
+//	Derek Slager
+//
+//	~ A Bassy Space Invaders 
+//	This really is just a porject for us to get started 
+*/
+
 #include <SDL.h>
 #include <SDL_Image.h>
 #include <SDL_ttf.h>
